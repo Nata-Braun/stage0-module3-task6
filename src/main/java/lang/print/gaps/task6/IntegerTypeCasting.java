29c4dbdf2744dae5e4e7d2f94b3b6cecbbecda23
Nata Braun <pushistik2030@gmail.com>
2023-02-02 20:56:54 +0300

    Task is solved

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
+    long first = 1000;
     public static void main(String[] args) {
+
+        long first = 1000;
+        var second = (int) first;
+        System.out.println(Long.toString(first) + "\n" + Integer.toString(second));
+
     }
 }
